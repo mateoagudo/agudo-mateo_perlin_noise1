e51b4217dc1ca2293cb6f3c8b108124849c3f025
mateoagudo <mateo.agudo@ucuenca.edu.ec>
2023-10-25 09:26:13 -0500

    se creó un array y un grupo de 10 pares de pelotas y lineas con diferente color alteran su posición y tamaño

--- sketch.js
+++ sketch.js
@@ -2,6 +2,21 @@ let particulas = [];
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  background(0);
+  fill(255);
+  noStroke();
+  textSize(14);
+  textStyle(BOLD);
+  text("CLICKEA PARA MÁS PLACER", 100, 100);
+  for (let i = 0; i < 10; i++) {
+    particula = new Particula();
+    particulas.push(particula);
+  }
 }
 
-function draw() {}
+function draw() {
+  for (let i = 0; i < particulas.length; i++) {
+    particulas[i].update();
+    particulas[i].display();
+  }
+}
